44c0c53e781b9853c87ede40ba0d383b178d4406
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-05 11:55:57 -0500

    :bookmark: v1.1.0

--- denoc/__init__.py
+++ denoc/__init__.py
@@ -5,7 +5,7 @@ This file defines denoc as a module.
 from .core import main
 
 __package_name__ = "denoc"
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 __authors__ = ["Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>"]
 __author_email__ = "eliaz.bobadilladev@gmail.com"
 __url__ = "https://github.com/UltiRequiem/denoc"
