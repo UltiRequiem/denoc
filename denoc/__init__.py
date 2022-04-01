e59ffaae7db8dc94553ec66beb11ba5b842c779a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-31 20:55:13 -0500

    :bookmark: 1.2.0

--- denoc/__init__.py
+++ denoc/__init__.py
@@ -3,9 +3,15 @@ This file defines denoc as a module.
 """
 
 from .core import main
+import warnings
 
 __package_name__ = "denoc"
 __version__ = "1.1.1"
 __authors__ = ["Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>"]
 __author_email__ = "eliaz.bobadilladev@gmail.com"
 __url__ = "https://github.com/UltiRequiem/denoc"
+
+warnings.warn(
+    "denoc is deprecated, check ulti.js.org/denoc for more information",
+    DeprecationWarning,
+)
